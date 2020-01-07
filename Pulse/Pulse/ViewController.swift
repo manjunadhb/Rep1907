e05f55d58c68b6127f7466a926cd6b680327e3c1
Himansu Panigrahi <54790637+himansudev@users.noreply.github.com>
2020-01-07 18:10:25 +0530

    Changed

--- Pulse/Pulse/ViewController.swift
+++ Pulse/Pulse/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //write code here
+        print("hello")
         // Do any additional setup after loading the view.
     }
 
